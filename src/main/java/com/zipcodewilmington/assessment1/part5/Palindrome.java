dc25f6ef51a58e93a642da68a6c1ce1cd296d1a0
Maira Botelho <mairabotelho@zipcoders-MacBook-Pro-3.local>
2019-10-28 16:21:59 -0400

    more changes

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -3,19 +3,32 @@ package com.zipcodewilmington.assessment1.part5;
 public class Palindrome {
 
     public Integer countPalindromes(String input){
+
         int count = 0;
-        String test = "";
-        int countSub = input.length();
 
-        for(int index = 0; index < input.length(); index++){
-            for(int index2 = index + 2; index2 <= input.length(); index2++){
+        boolean[][] arr = new boolean[input.length()][input.length()];
 
-                test = input.substring(index, index2);
-                countSub++;
+        for (int index = 0; index < input.length(); index++) {
+            arr[index][index] = true;
+            count++;
+        }
+
+        for (int index = 1; index < input.length(); index++) {
+            if (input.charAt(index - 1) == input.charAt(index)) {
+                arr[index - 1][index] = true;
+                count++;
             }
         }
 
-        return countSub;
+        for (int index = 2; index < input.length(); index++) {
+            for (int index2 = 0; index2 < index; index2++) {
+                if (arr[index2 + 1][index - 1] && input.charAt(index2) == input.charAt(index)) {
+                    arr[index2][index] = true;
+                    count++;
+                }
+            }
+        }
+        return count;
 
     }
 }
